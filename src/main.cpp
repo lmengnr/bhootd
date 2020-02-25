2776ef927f865db560924aea5391e0e936df939b
lmengnr <fahd.imtiaz.bhatti@gmail.com>
2020-02-25 22:17:48 +0500

    reading from stat file implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include "json.hpp";
+#include <fstream>
 
+using json = nlohmann::json;
 
 int main(int argc, char *argv[]) {
   
   system("mpstat -o JSON > stat.json");
+
+  std::ifstream stats("stat.json");
+  
+  if (!stats){
+    std::cout<< "Failed to read" <<std::endl;
+  }
+
+  
+
 
 }
