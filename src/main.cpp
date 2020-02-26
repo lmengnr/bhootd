a3f132c612fe5882b3819bda63bb313df02a21a9
lmengnr <fahd.imtiaz.bhatti@gmail.com>
2020-02-26 18:45:58 +0500

    spacings

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include "main.hpp"
 
-
 int main() {
 
-  for (int i=0; i <10; i++){
-
+  for (int i = 0; i < 10; i++) {
   };
-  
+
   cpu_usage A;
   std::cout << "Node ID: " << A.node_id << std::endl;
   std::cout << "Current CPU Usage: " << A.currValue << std::endl;
@@ -15,7 +13,6 @@ int main() {
   std::string stat_line;
   std::getline(filestat, stat_line);
 
-  std::cout<< "Full CPU Stats" <<std::endl;
-  std::cout<< stat_line <<std::endl;
-
+  std::cout << "Full CPU Stats" << std::endl;
+  std::cout << stat_line << std::endl;
 }
