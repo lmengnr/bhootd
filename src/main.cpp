a5f3439796e3d3dcb8940a62c29c9f3d7cc61b95
lmengnr <fahd.imtiaz.bhatti@gmail.com>
2020-02-26 16:49:58 +0500

    changing to OOP implementation

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,64 @@
 
 using json = nlohmann::json;
 
+class cpu_usage {
+
+public:
+  double currValue;
+  std::string node_id;
+
+  cpu_usage(){
+    currValue = get_curr_val();
+  }
+
+private:
+  u_int lastTotalUser, lastTotalUserLow, lastTotalSys, lastTotalIdle;
+
+  double get_curr_val() {
+    u_int totalUser, totalUserLow, totalSys, totalIdle, total;
+    double percent;
+
+    std::ifstream filestat("/proc/stat");
+    std::string stat_line;
+    std::getline(filestat, stat_line);
+
+    std::cout << stat_line << std::endl;
+
+    std::string stat_line2 = stat_line.substr(5, stat_line.find(" 0 0"));
+
+    std::stringstream statstream;
+    statstream << stat_line2;
+
+    statstream >> totalUser;
+    statstream >> totalUserLow;
+    statstream >> totalSys;
+    statstream >> totalIdle;
+    statstream >> total;
+
+    if (totalUser < lastTotalUser || totalUserLow < lastTotalUserLow ||
+        totalSys < lastTotalSys || totalIdle < lastTotalIdle) {
+      // Overflow detection. Just skip this value.
+      percent = -1.0;
+    } else {
+      total = (totalUser - lastTotalUser) + (totalUserLow - lastTotalUserLow) +
+              (totalSys - lastTotalSys);
+      percent = total;
+      total += (totalIdle - lastTotalIdle);
+      percent /= total;
+      percent *= 100;
+    }
+
+    return percent;
+  }
+};
+
 u_int lastTotalUser, lastTotalUserLow, lastTotalSys, lastTotalIdle;
 
 int main(int argc, char *argv[]) {
 
+  std::string name;
+  
+  
   u_int totalUser, totalUserLow, totalSys, totalIdle, total;
   double percent;
 
