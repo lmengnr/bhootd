1fa6412a695fa11c23731597c6b1a9d6031fe251
lmengnr <fahd.imtiaz.bhatti@gmail.com>
2020-02-26 17:48:19 +0500

    Displaying after OOP implementation

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,10 @@ public:
   double currValue;
   std::string node_id;
 
-  cpu_usage() { currValue = get_curr_val();
-  getHostname(node_id);
-   }
+  cpu_usage() {
+    currValue = get_curr_val();
+    getHostname(node_id);
+  }
 
 private:
   u_int lastTotalUser, lastTotalUserLow, lastTotalSys, lastTotalIdle;
@@ -27,7 +28,6 @@ private:
     std::ifstream filestat("/proc/stat");
     std::string stat_line;
     std::getline(filestat, stat_line);
-
 
     std::string stat_line2 = stat_line.substr(5, stat_line.find(" 0 0"));
 
@@ -56,57 +56,29 @@ private:
     return percent;
   }
 
-  void getHostname(std::string & hostname) {
-    
+  void getHostname(std::string &hostname) {
+
     std::ifstream name("/etc/hostname");
     std::getline(name, hostname);
   }
 };
 
-u_int lastTotalUser, lastTotalUserLow, lastTotalSys, lastTotalIdle;
 
-int main(int argc, char *argv[]) {
+int main() {
 
+  for (int i=0; i <10; i++){
+
+  };
+  
   cpu_usage A;
-  std::cout<< "Node ID: "<< A.node_id << std::endl;
-  std::cout<< "Current CPU Usage: " << A.currValue << std::endl;
-  
-  
-  u_int totalUser, totalUserLow, totalSys, totalIdle, total;
-  double percent;
+  std::cout << "Node ID: " << A.node_id << std::endl;
+  std::cout << "Current CPU Usage: " << A.currValue << std::endl;
 
   std::ifstream filestat("/proc/stat");
   std::string stat_line;
   std::getline(filestat, stat_line);
 
-  std::cout << stat_line << std::endl;
+  std::cout<< "Full CPU Stats" <<std::endl;
+  std::cout<< stat_line <<std::endl;
 
-  std::string stat_line2 = stat_line.substr(5, stat_line.find(" 0 0"));
-
-  // std::cout<< stat_line2 <<std::endl;
-
-  std::stringstream statstream;
-  statstream << stat_line2;
-
-  std::string s1, s2, s3, s4;
-  statstream >> totalUser;
-  statstream >> totalUserLow;
-  statstream >> totalSys;
-  statstream >> totalIdle;
-  statstream >> total;
-
-  if (totalUser < lastTotalUser || totalUserLow < lastTotalUserLow ||
-      totalSys < lastTotalSys || totalIdle < lastTotalIdle) {
-    // Overflow detection. Just skip this value.
-    percent = -1.0;
-  } else {
-    total = (totalUser - lastTotalUser) + (totalUserLow - lastTotalUserLow) +
-            (totalSys - lastTotalSys);
-    percent = total;
-    total += (totalIdle - lastTotalIdle);
-    percent /= total;
-    percent *= 100;
-  }
-
- // std::cout<< percent <<std::endl;
 }
