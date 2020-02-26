fdbb7af2d5a11b2c6a881f7facf1bbb60de31506
lmengnr <fahd.imtiaz.bhatti@gmail.com>
2020-02-26 19:07:46 +0500

    breaking build

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,11 @@
 int main() {
 
   for (int i = 0; i < 10; i++) {
-  };
+  }
 
   cpu_usage A;
   std::cout << "Node ID: " << A.node_id << std::endl;
-  std::cout << "Current CPU Usage: " << A.currValue << std::endl;
+  std::cout << "Current CPU Usage: " << A.currValue << std::endl
 
   std::ifstream filestat("/proc/stat");
   std::string stat_line;
