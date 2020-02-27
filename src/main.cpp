5df95eb883e3b9d04b67a5938e56d73c4abce803
lmengnr <fahd.imtiaz.bhatti@gmail.com>
2020-02-27 23:30:33 +0500

    takes node id as input and other stuff

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,9 @@ int main(int argc, char **argv) {
   po::options_description desc("Allowed Options");
   desc.add_options()("help", "Print help messages")(
       "broker-addr", po::value<std::string>(),
-      "<IP address of broker e.g. 127.0.0.1>")("skip", "display stats");
+      "<IP address of broker e.g. 127.0.0.1>")(
+      "node-id", po::value<std::string>(), "ID of this node")("see-stats",
+                                                              "display stats");
 
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -22,19 +24,22 @@ int main(int argc, char **argv) {
     std::cout << "Address Provided is: " << vm["broker-addr"].as<std::string>()
               << std::endl;
 
-  } else if (vm.count("skip")) {
+  } else if (vm.count("see-stats")) {
     // start displaying stats
-    // This option is just temporary. Remove this later
+  }
+  if (vm.count("node-id")) {
+    std::cout << "ID of the Node is: " << vm["node-id"].as<std::string>()
+              << std::endl;
   } else {
     std::cout << "No address provided" << std::endl << desc << std::endl;
     return 1;
   }
 
-  cpu_usage orb("1");
+  CpuUsage orb(vm["node-id"].as<std::string>());
+  std::stringstream json_out;
 
   while (1) {
 
-    std::stringstream json_out;
     json_out << "{ \"node_id\": " << orb.node_id
              << ", \"cpu_usage\": " << orb.get_curr_val() << " }";
 
