e4eff08bd6d95c5c5596740e9314e763d962868b
lmengnr <fahd.imtiaz.bhatti@gmail.com>
2020-03-01 14:48:06 +0500

    dump this

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv) {
   namespace po = boost::program_options;
 
   po::options_description desc("Allowed Options");
-  desc.add_options()("help", "Print help messages")(
+  desc.add_options()("help", "Print help messages")("skip", "Show stats")(
       "broker-addr", po::value<std::string>(),
       "<IP address of broker e.g. 127.0.0.1>")(
       "node-id", po::value<std::string>(), "ID of this node");
@@ -15,6 +15,11 @@ int main(int argc, char **argv) {
   po::store(po::parse_command_line(argc, argv, desc), vm);
 
   bool addr_provided = 0;
+  bool simple_out = 0;
+
+  if (vm.count("skip")) {
+    simple_out = 1;
+  }
 
   if (vm.count("help")) {
     std::cout << "bhootd is a daemon that runs on nodes and sends CPU usage "
@@ -32,18 +37,32 @@ int main(int argc, char **argv) {
   if (vm.count("node-id")) {
     std::cout << "ID of the Node is: " << vm["node-id"].as<std::string>()
               << std::endl;
-  } else {
-    std::cout << "No address provided" << std::endl << desc << std::endl;
-    return 1;
   }
-
-  // If there was no address then stop
-  if (!addr_provided)
-    return 1;
 
   CpuUsage orb(vm["node-id"].as<std::string>());
   std::stringstream json_out;
   std::string json_str = json_out.str();
+
+  // if see-stat option is provided then simply display stats on the terminal
+
+  /* if (simple_out) {
+
+     while (1) {
+
+       json_out.str(std::string());
+       json_out << "{ \"node_id\": \"" << orb.node_id
+                << "\", \"cpu_usage\": " << orb.get_curr_val() << " }";
+
+       std::cout << json_out.str() << std::endl;
+       sleep(1);
+     }
+   }*/
+
+  // If there was no address then exit with error
+  if (!addr_provided) {
+    std::cerr << "No address provided" << std::endl << desc << std::endl;
+    return 1;
+  }
 
   std::string address = vm["broker-addr"].as<std::string>();
   mqtt::async_client cli(address, "");
